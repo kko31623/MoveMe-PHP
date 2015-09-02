84dd48511bb8cc0ca0b82c974cab348d8a47129c
systemAce24185 <krishoza@Krishs-MBP.home>
2015-09-02 10:17:18 +0100

    styled

--- houses.php
+++ houses.php
@@ -1,3 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/skeleton/2.0.4/skeleton.min.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/stylesheet.css">
+	<title></title>
+</head>
+<body>
+<div id="img">
+<img src="assets/images/logo.jpeg"/>
+</div>
+<div class="main">
+
 <?php
 	function find($location){
 		$ch = curl_init();
@@ -14,13 +27,12 @@
   		$listData = array();
   		if (isset($data->error_code)){
   			echo "
-  				<h3>Sorry but this location is unavailable</h3>";		
+  				<h3 style='text-align:center'>Sorry but this location is unavailable</h3>";		
   		}
   		else{
 	  		foreach ($data->listing as $item) { 
 	  			$listData[] = array(
 	  				'numBedrooms' => $item->num_bedrooms,
-	  				'area'=> $data->area_name,
 	  				'price'=> $item->price,
 	  				'description'=> $item->description,
 	  				'county' => $item->county,
@@ -35,25 +47,18 @@
 	$results = find($_POST['location']);
 ?>
 
-<!DOCTYPE html>
-<html>
-<head>
-	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/skeleton/2.0.4/skeleton.min.css">
-	<link rel="stylesheet" type="text/css" href="assets/css/stylesheet.css">
-	<title></title>
-</head>
-<body>
 	<?php if(count($results) > 0 ): ?>
 		<?php foreach($results as $result): ?>
-			<h5>Area: <?= $result['area']?></h5>
-			<h6>County: <?= $result['county']?></h6>
+			<h5>County: <?= $result['county']?></h5>
 			<p>Address: <?= $result['address']?></p>
 			<p>Property Type: <?= $result['propertyType']?></p>		
 			<p>Number of Bedrooms: <?= $result['numBedrooms']?></p>
 			<p>Price: £<?= $result['price']?></p>
-			<p>Description: <?= $result['description']?></p>
+			<p>Description: <?= $result['description']?></p><br><br>
+			<p>_________________________________________________________________________________________________________________</p>
 		<?php endforeach; ?>
 	<?php endif; ?>
+</div>
 </body>
 </html>
 
