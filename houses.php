5f10dcf2bd45b6ef42d63de0797d26f59513824c
systemAce24185 <krishoza@Krishs-MBP.home>
2015-09-26 20:46:25 +0100

    fixed da stupid indentations

--- houses.php
+++ houses.php
@@ -1,86 +1,86 @@
 <!DOCTYPE html>
 <html>
-<head>
-	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/skeleton/2.0.4/skeleton.min.css">
-	<link rel="stylesheet" type="text/css" href="assets/css/stylesheet.css">
-	<title></title>
-</head>
-<body>
-<div id="img">
-<img src="assets/images/logo.jpeg"/>
-</div>
-<div class="main">
+	<head>
+		<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/skeleton/2.0.4/skeleton.min.css">
+		<link rel="stylesheet" type="text/css" href="assets/css/stylesheet.css">
+		<title></title>
+	</head>
+	<body>
+	<div id="img">
+	<img src="assets/images/logo.jpeg"/>
+	</div>
+	<div class="main">
 
-<?php
-	function getCrime($lat, $lng){
-		$ch = curl_init();
-		curl_setopt($ch, CURLOPT_URL, "https://data.police.uk/api/crimes-at-location?lng=" . $lng . "&lat=" . $lat . "");
-		curl_setopt($ch, CURLOPT_HTTPHEADER, array('accept: text/json'));
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-		$json = curl_exec($ch);
-		curl_close($ch);
-		$data = json_decode($json);
-	//	return count($data);
-		echo count($data);
-	}
-	function find($location){
-		$ch = curl_init();
-
-		curl_setopt($ch, CURLOPT_URL, "http://api.zoopla.co.uk/api/v1/property_listings.json?area=" . $location . "&api_key=c88442waawanw44ygzem2ee2");
-	  	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-  		$json = curl_exec($ch);
-  		curl_close($ch);      
-  
-  		$data = json_decode($json);
-  		//var_dump($data);
-
-  		$listData = array();
-  		if (isset($data->error_code) || count($data) <= 0){
-  			echo "
-  				<h3 style='text-align:center'>Sorry but this location is unavailable</h3>";		
-  		}
-  		else{
-	  		foreach ($data->listing as $item) { 
-	  			$listData[] = array(
-	  				'numBedrooms' => $item->num_bedrooms,
-	  				'price'=> $item->price,
-	  				'description'=> $item->description,
-	  				'county' => $item->county,
-	  				'propertyType' => $item->property_type,
-	  				'address' => $item->displayable_address,
-	  				'lng' => $item->longitude,
-	  				'lat' => $item->latitude
-	  			);
-	  		}
-			//var_dump($listData);
-			return $listData;
+	<?php
+		function getCrime($lat, $lng){
+			$ch = curl_init();
+			curl_setopt($ch, CURLOPT_URL, "https://data.police.uk/api/crimes-at-location?lng=" . $lng . "&lat=" . $lat . "");
+			curl_setopt($ch, CURLOPT_HTTPHEADER, array('accept: text/json'));
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			$json = curl_exec($ch);
+			curl_close($ch);
+			$data = json_decode($json);
+		//	return count($data);
+			echo count($data);
 		}
-	}
-	$results = find($_POST['location']);
-?>
+		function find($location){
+			$ch = curl_init();
 
-	<?php if(count($results) > 0 ): ?>
-		<?php foreach($results as $result): ?>
-			<h5>Area: <?= $result['county']?></h5>
-			<p>Address: <?= $result['address']?></p>
-			<p>Property Type: <?= $result['propertyType']?></p>		
-			<p>Number of Bedrooms: <?= $result['numBedrooms']?></p>
-			<p>Price: <?php
-			 	if($result['price'] != "0"){
-			 		echo("£" . $result['price']);
-			 	}
-			 	else{
-			 		echo"TBC";
-			 	}
-			 
-			 ?></p>
-			<p>Description: <?= $result['description']?></p><br><br>
-			<p>Crime Count: <?= getCrime($result['lat'], $result['lng']);?></p>
-			<p>_________________________________________________________________________________________________________________</p>
-		<?php endforeach; ?>
-	<?php endif; ?>
-</div>
-</body>
+			curl_setopt($ch, CURLOPT_URL, "http://api.zoopla.co.uk/api/v1/property_listings.json?area=" . $location . "&api_key=c88442waawanw44ygzem2ee2");
+		  	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	  		$json = curl_exec($ch);
+	  		curl_close($ch);      
+	  
+	  		$data = json_decode($json);
+	  		//var_dump($data);
+
+	  		$listData = array();
+	  		if (isset($data->error_code) || count($data) <= 0){
+	  			echo "
+	  				<h3 style='text-align:center'>Sorry but this location is unavailable</h3>";		
+	  		}
+	  		else{
+		  		foreach ($data->listing as $item) { 
+		  			$listData[] = array(
+		  				'numBedrooms' => $item->num_bedrooms,
+		  				'price'=> $item->price,
+		  				'description'=> $item->description,
+		  				'county' => $item->county,
+		  				'propertyType' => $item->property_type,
+		  				'address' => $item->displayable_address,
+		  				'lng' => $item->longitude,
+		  				'lat' => $item->latitude
+		  			);
+		  		}
+				//var_dump($listData);
+				return $listData;
+			}
+		}
+		$results = find($_POST['location']);
+	?>
+
+		<?php if(count($results) > 0 ): ?>
+			<?php foreach($results as $result): ?>
+				<h5>Area: <?= $result['county']?></h5>
+				<p>Address: <?= $result['address']?></p>
+				<p>Property Type: <?= $result['propertyType']?></p>		
+				<p>Number of Bedrooms: <?= $result['numBedrooms']?></p>
+				<p>Price: <?php
+				 	if($result['price'] != "0"){
+				 		echo("£" . $result['price']);
+				 	}
+				 	else{
+				 		echo"TBC";
+				 	}
+				 
+				 ?></p>
+				<p>Description: <?= $result['description']?></p><br><br>
+				<p>Crime Count: <?= getCrime($result['lat'], $result['lng']);?></p>
+				<p>_________________________________________________________________________________________________________________</p>
+			<?php endforeach; ?>
+		<?php endif; ?>
+	</div>
+	</body>
 </html>
 
 
